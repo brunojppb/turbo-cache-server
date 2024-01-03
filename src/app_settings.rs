25bc5ed172d963612556fdbae0be43a0f4bf107c
Bruno Paulino <bruno.paulino@n26.com>
2024-01-03 15:09:08 +0100

    Simplify app config

--- src/app_settings.rs
+++ src/app_settings.rs
@@ -1,24 +1,15 @@
-use serde_aux::field_attributes::deserialize_number_from_string;
-#[derive(Clone, serde::Deserialize)]
+use std::env;
+
+#[derive(Clone)]
 pub struct AppSettings {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
 }
 
-pub fn get_settings() -> Result<AppSettings, config::ConfigError> {
-    let settings = config::Config::builder()
-        .add_source(config::File::new("config.yaml", config::FileFormat::Yaml))
-        .add_source(
-            // This allows us to overwrite any of our config values
-            // using environment variables starting with APP_
-            // For instance, to overwrite the port number, we can pas
-            // in the following variable:
-            // APP__PORT=3000
-            config::Environment::with_prefix("APP")
-                .prefix_separator("_")
-                .separator("__"),
-        )
-        .build()?;
+pub fn get_settings() -> AppSettings {
+    let port = env::var("PORT")
+        .unwrap_or("8000".to_string())
+        .parse::<u16>()
+        .expect("Could not read PORT from env");
 
-    settings.try_deserialize::<AppSettings>()
+    AppSettings { port }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use decay::app_settings::get_settings;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app_settings = get_settings().expect("Failed to read config");
+    let app_settings = get_settings();
     let address = format!("127.0.0.1:{}", app_settings.port);
     let listener = TcpListener::bind(address)?;
     decay::startup::run(listener)?.await
