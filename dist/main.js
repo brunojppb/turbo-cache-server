919c34d2ca8b54ac71e5b7e977ca5a4d3f2ff2a3
Bruno Paulino <bruno@bpaulino.com>
2024-01-16 23:32:06 +0100

    Make sure logs output path exists (#15)

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,13 @@
 const { spawn } = require("node:child_process");
 const { resolve } = require("node:path");
-const { createWriteStream } = require('node:fs')
+const { createWriteStream, mkdirSync, existsSync } = require('node:fs')
 const { DECAY_PID_KEY, TEMP_DIR, saveState } = require("./util");
 
 const serverBinary = resolve(__dirname, "./decay");
+
+if (!existsSync(TEMP_DIR)) {
+  mkdirSync(TEMP_DIR, { recursive: true });
+}
 
 // @TODO: Check whether I actually want to give args to the binary on startup
 const decayProcess = spawn(serverBinary, [/** input here as args once we suppor them */], {
