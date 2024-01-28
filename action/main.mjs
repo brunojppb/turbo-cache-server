5e6d697a9edb5c0016b9e9f0240a3eb77bae22df
Bruno Paulino <bruno@bpaulino.com>
2024-01-28 11:06:27 +0100

    Remove ignore

--- action/main.mjs
+++ action/main.mjs
@@ -20,7 +20,6 @@ const errOutput = createWriteStream(resolve(TEMP_DIR, "error.log"), {
 
 const decayProcess = spawn(serverBinary, [], {
   detached: true,
-  stdio: "ignore",
   env: {
     ...process.env,
   },
