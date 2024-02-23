916f419f069af22b6f47276e72eaaa85c9d4803c
Bruno Paulino <bruno@bpaulino.com>
2024-02-23 09:39:42 +0100

    Tracing and logging (#19)
    * Convert action to modules
    
    * Clean-up action file
    
    * Clean-up action file
    
    * Clean-up action file
    
    * output server startup
    
    * Read output logs in sync mode
    
    * test binary
    
    * Fix import path
    
    * ignore stdio
    
    * Exit process
    
    * Remove ignore
    
    * Exit on post action
    
    * Debug output paths
    
    * Touch output files
    
    * Try to redirect ouput
    
    * Pass logging directory as env to server binary
    
    * Enable Telemetry and optional file logging
    
    * Instrument endpoints
    
    * Update action description
    
    * Test new binary
    
    * Wrap Actix server with default logger
    
    * Test new binary
    
    * Remove temporary binary
    
    * Update README

---
+++ src/telemetry.rs
@@ -0,0 +1,48 @@
+use std::env;
+
+use tracing::{subscriber::set_global_default, Subscriber};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{
+    fmt::{self, MakeWriter},
+    layer::SubscriberExt,
+    EnvFilter,
+};
+
+pub fn get_telemetry_subscriber<Sink>(
+    name: &str,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name.into(), sink);
+
+    // Only output logs to a file if the runtime has given an output path
+    let maybe_file_layer = match env::var("LOGS_DIRECTORY") {
+        Ok(logs_dir) => {
+            let file_appender =
+                tracing_appender::rolling::never(logs_dir, format!("{}.log", &name));
+            let file_layer = fmt::layer().with_writer(file_appender);
+            Some(file_layer)
+        }
+        Err(_) => None,
+    };
+
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+        .with(maybe_file_layer)
+}
+
+/// Initialise the telemetry stack by setting up the global
+/// default telemetry subscriber. The subscriber will handle log and tracing
+/// events based on the pre-configured layers.
+pub fn init_telemetry_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Could not set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
