fc584fbb8b63bf9ab18c83e97723f0a891ac7e9b
Bruno Paulino <bruno@bpaulino.com>
2024-02-23 08:21:34 +0100

    Pass logging directory as env to server binary

--- action/main.mjs
+++ action/main.mjs
@@ -1,34 +1,26 @@
-import { spawn } from "node:child_process";
-import { resolve } from "node:path";
-import { URL } from "node:url";
-import {
-  createWriteStream,
-  mkdirSync,
-  existsSync,
-  writeFileSync,
-} from "node:fs";
-import { DECAY_PID_KEY, TEMP_DIR, saveState } from "./util.mjs";
+import { spawn } from 'node:child_process'
+import { resolve } from 'node:path'
+import { LOGS_DIR, DECAY_PID_KEY, saveState } from './util.mjs'
 
-const __dirname = new URL(".", import.meta.url).pathname;
-const serverBinary = resolve(__dirname, "./decay");
+const __dirname = new URL('.', import.meta.url).pathname
+const serverBinary = resolve(__dirname, './decay')
 
-if (!existsSync(TEMP_DIR)) {
-  mkdirSync(TEMP_DIR, { recursive: true });
-}
-
-const outFile = resolve(TEMP_DIR, "out.log");
-
-const decayProcess = spawn(serverBinary, [">", outFile, "2>&1"], {
+const decayProcess = spawn(serverBinary, [], {
   detached: true,
   env: {
     ...process.env,
+    LOGS_DIRECTORY: LOGS_DIR,
   },
-});
+})
 
-decayProcess.unref();
+decayProcess.unref()
 
-const pid = decayProcess.pid?.toString();
+const pid = decayProcess.pid?.toString()
 
-console.log("Decay server running with pid: ", pid);
-saveState(DECAY_PID_KEY, pid);
-process.exit(0);
+console.log(`
+Turbo Cache Server running with pid: "${pid}"
+Web server logs are being written at "${LOGS_DIR}"
+`)
+
+saveState(DECAY_PID_KEY, pid)
+process.exit(0)
--- action/post.mjs
+++ action/post.mjs
@@ -1,28 +1,24 @@
-import path from "node:path";
-import fs from "node:fs";
-import { getState, DECAY_PID_KEY, TEMP_DIR } from "./util.mjs";
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { getState, LOGS_DIR, DECAY_PID_KEY } from './util.mjs'
 
-const pid = getState(DECAY_PID_KEY);
+const pid = getState(DECAY_PID_KEY)
 
-if (typeof pid === "undefined") {
-  console.error(`${DECAY_PID_KEY} state could not be found`);
-  process.exit(1);
+if (typeof pid === 'undefined') {
+  console.error(`${DECAY_PID_KEY} state could not be found`)
+  process.exit(1)
 }
 
 // @TODO: Check whether the server is actually running
-console.log(`Decay server will be stopped on pid: ${pid}`);
-process.kill(parseInt(pid));
+// Before sending a SIGTERM.
+console.log(`Turbo Cache Server will be stopped on pid: ${pid}`)
+process.kill(parseInt(pid))
 
-const stdLogs = fs.readFileSync(path.resolve(TEMP_DIR, "out.log"), {
-  encoding: "utf8",
-  flag: "r",
-});
+// Read logs and output it as-is so we can debug
+// any potential errors during the Turborepo remote cache API calls
+const logFile = path.resolve(LOGS_DIR, 'decay.log')
+console.log(`Reading Turbo Cache Server logs from ${logFile}`)
+const serverLogs = fs.readFileSync(logFile, { encoding: 'utf-8' })
+console.log(serverLogs)
 
-// const errLogs = fs.readFileSync(path.resolve(TEMP_DIR, "error.log"), {
-//   encoding: "utf8",
-//   flag: "r",
-// });
-
-console.log(`Server output: `, stdLogs);
-console.error(`\n\nServer errors: `, errLogs);
-process.exit(0);
+process.exit(0)
