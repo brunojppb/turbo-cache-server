15caa3705a79d433ff1cfd8a127c01a3ee982d1e
Nicolai Hornskov Nielsen <nicolai.nielsen@lego.com>
2024-02-26 10:07:04 +0100

    chore: added debug check to process kill on post

--- action/post.mjs
+++ action/post.mjs
@@ -1,25 +1,30 @@
-import * as fs from 'node:fs'
-import * as path from 'node:path'
-import { getState, LOGS_DIR, DECAY_PID_KEY } from './util.mjs'
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { getState, LOGS_DIR, DECAY_PID_KEY } from "./util.mjs";
 
-const pid = getState(DECAY_PID_KEY)
+const pid = getState(DECAY_PID_KEY);
 
-if (typeof pid === 'undefined') {
-  console.error(`${DECAY_PID_KEY} state could not be found`)
-  process.exit(1)
+if (typeof pid === "undefined") {
+  console.error(`${DECAY_PID_KEY} state could not be found`);
+  process.exit(1);
 }
 
 // @TODO: Check whether the server is actually running
 // Before sending a SIGTERM.
-console.log(`Turbo Cache Server will be stopped on pid: ${pid}`)
-process.kill(parseInt(pid))
+console.log(`Turbo Cache Server will be stopped on pid: ${pid}`);
+try {
+  process.kill(parseInt(pid));
+} catch (err) {
+  console.error(`Error stopping Turbo Cache Server: ${err.message}`);
+  process.exit(1);
+}
 
 // Read logs and output it as-is so we can debug
 // any potential errors during the Turborepo remote cache API calls.
 // Logs are written on a "{crate_name}.log" file
-const logFile = path.resolve(LOGS_DIR, 'decay.log')
-console.log(`Reading Turbo Cache Server logs from ${logFile}`)
-const serverLogs = fs.readFileSync(logFile, { encoding: 'utf-8' })
-console.log(serverLogs)
+const logFile = path.resolve(LOGS_DIR, "decay.log");
+console.log(`Reading Turbo Cache Server logs from ${logFile}`);
+const serverLogs = fs.readFileSync(logFile, { encoding: "utf-8" });
+console.log(serverLogs);
 
-process.exit(0)
+process.exit(0);
