67a5167d59ab8c8249a88087dc0544c79d0e63c9
Bruno Paulino <bruno@bpaulino.com>
2026-08-23 13:31:52 +0200

    feat: add ArtifactCache use-case behind an ArtifactStore port

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,4 @@ pub mod routes;
 pub mod startup;
 pub mod storage;
 pub mod telemetry;
+pub mod usecases;
---
+++ src/usecases/mod.rs
@@ -0,0 +1,5 @@
+pub mod artifact_cache;
+pub mod store;
+
+pub use artifact_cache::ArtifactCache;
+pub use store::ArtifactStore;
